3ce2e5369682483e34b1b9ddf450f0bce842963d
Ironakatina <116155572+Ironakatina33@users.noreply.github.com>
2024-02-07 12:53:55 +0100

    g

--- code.js
+++ code.js
@@ -16,3 +16,41 @@ function checkCode() {
         alert('Code incorrect. Veuillez réessayer.');
     }
 }
+
+function limitInputLength2() {
+    var codeInput = document.getElementById('codeInput');
+    if (codeInput.value.length > 4) {
+        codeInput.value = codeInput.value.slice(0, 4); // Limiter la saisie à 4 caractères
+    }
+}
+
+function checkCode2() {
+    var enteredCode = document.getElementById('codeInput').value;
+
+    // Ajoutez votre logique pour vérifier le code ici
+    if (enteredCode === '5678') {
+        // Redirigez l'utilisateur vers la page suivante
+        window.location.href = '../page/Epreuve2Page3.html';
+    } else {
+        alert('Code incorrect. Veuillez réessayer.');
+    }
+}
+
+function limitInputLength3() {
+    var codeInput = document.getElementById('codeInput');
+    if (codeInput.value.length > 4) {
+        codeInput.value = codeInput.value.slice(0, 4); // Limiter la saisie à 4 caractères
+    }
+}
+
+function checkCode3() {
+    var enteredCode = document.getElementById('codeInput').value;
+
+    // Ajoutez votre logique pour vérifier le code ici
+    if (enteredCode === '6464') {
+        // Redirigez l'utilisateur vers la page suivante
+        window.location.href = '../page/Epreuve3Page3.html';
+    } else {
+        alert('Code incorrect. Veuillez réessayer.');
+    }
+}
---
+++ popup.js
@@ -0,0 +1,10 @@
+
+function openPopup() {
+    document.getElementById('popup-container').style.display = 'block';
+    document.getElementById('overlay').style.display = 'block';
+}
+
+function closePopup() {
+    document.getElementById('popup-container').style.display = 'none';
+    document.getElementById('overlay').style.display = 'none';
+}
