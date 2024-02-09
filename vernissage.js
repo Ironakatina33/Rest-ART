dc0495c61a68722afd28e52b22e8919f01a32f58
Ironakatina <116155572+Ironakatina33@users.noreply.github.com>
2024-02-09 12:50:41 +0100

    q

--- vernissage.js
+++ vernissage.js
@@ -11,8 +11,8 @@ const brush = new PIXI.Graphics()
 // Create a line that will interpolate the drawn points
 const line = new PIXI.Graphics();
 
-PIXI.Assets.add('t1', '../images/wbep/nettoyage-cadreweb.webp');
-PIXI.Assets.add('t2', '../images/wbep/grattage-cadreweb.webp');
+PIXI.Assets.add('t1', '../images/wbep/vernissage-cadreweb.webp');
+PIXI.Assets.add('t2', '../images/wbep/resultat-final.webp');
 PIXI.Assets.load(['t1', 't2']).then(setup);
 
 function setup()
