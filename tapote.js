2c0b8fff99a858f3d6922176d82abfef5eba365a
Ironakatina <116155572+Ironakatina33@users.noreply.github.com>
2024-02-12 18:55:15 +0100

    Update tapote.js

--- tapote.js
+++ tapote.js
@@ -14,7 +14,7 @@ const brush = new PIXI.Graphics()
 // Create a line that will interpolate the drawn points
 const line = new PIXI.Graphics();
 
-PIXI.Assets.add('t1', '../images/wbep/nettoyage-cadreweb.webp');
+PIXI.Assets.add('t1', '../Rest-ART/images/wbep/nettoyage-cadreweb.webp');
 PIXI.Assets.add('t2', '../images/wbep/grattage-cadreweb.webp');
 PIXI.Assets.load(['t1', 't2']).then(setup);
 
